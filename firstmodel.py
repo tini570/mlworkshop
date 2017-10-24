40bd62f959a6b4ff7a7dc28bc71341d588984e39
tini570 <tinith570@gmail.com>
2017-10-24 16:41:09 +0530

    firstcommit

---
+++ firstmodel.py
@@ -0,0 +1,9 @@
+from sklearn.tree import DecisionTreeClassifier
+features =[[140,0],[130,0],[150,1],[170,1]]
+labels=[0,0,1,1]
+
+clf=DecisionTreeClassifier()
+clf.fit(features,labels)
+
+p=clf.predict([[160,1]])
+print("Prediction =",p)
---
+++ secondmodel.py
@@ -0,0 +1,10 @@
+from sklearn.tree import DecisionTreeClassifier
+
+X=[[181,80,44],[177,70,43],[160,60,38],[154,54,37],[166,65,40],[190,90,47],[175,64,39],[177,70,40],[159,55,37],[171,75,42],[181,85,43]]
+Y=['male','male','female','female','male','male','female','female','female','male','male']
+
+clf=DecisionTreeClassifier()
+clf.fit(X,Y)
+
+p=clf.predict([[190,70,43]])
+print("Prediction=",p)
